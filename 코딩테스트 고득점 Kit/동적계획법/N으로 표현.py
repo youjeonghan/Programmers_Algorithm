4c7bceec7c7fd2ed2371dabe18e2b1e45512cd5c
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-03 15:38:47 +0900

    타겟 넘버, 네트워크, N으로 표현

---
+++ 코딩테스트 고득점 Kit/동적계획법/N으로 표현.py
@@ -0,0 +1,8 @@
+# 3레벨     코딩테스트 고득점 Kit     N으로 표현
+def solution(N, number):
+    dp = [1, 2, 3, 4, 5, -5, -4, -3, -2, -1]
+    print(dp[-2])
+
+
+if __name__ == "__main__":
+    print(solution(5, 12))
