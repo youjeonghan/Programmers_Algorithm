77f74a23be6d676bbc8c2d5f4346127f5c4ba05c
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-04-14 18:42:34 +0900

    등굣길

--- 코딩테스트 고득점 Kit/동적계획법/정수 삼각형.py
+++ 코딩테스트 고득점 Kit/동적계획법/정수 삼각형.py
@@ -1,46 +1,16 @@
 # 3레벨     코딩테스트 고득점 Kit     정수 삼각형
-
-
 def solution(triangle):
-    _max = 0
-    for floor, list in enumerate(triangle):
-        if floor == len(triangle) - 1:
-            break
-        for idx, item in enumerate(list):
+    for floor in range(1, len(triangle)):
+        for idx in range(len(triangle[floor])):
             if idx == 0:
-                triangle[floor + 1][idx] += item
-            elif idx == len(list) - 2:
-                triangle[floor + 1][idx + 1] += item
+                triangle[floor][idx] += triangle[floor - 1][idx]
+            elif idx == len(triangle[floor]) - 1:
+                triangle[floor][idx] += triangle[floor - 1][idx - 1]
             else:
-                triangle[floor + 1][idx] += item
-                triangle[floor + 1][idx + 1] += item
+                triangle[floor][idx] += max(triangle[floor - 1][idx], triangle[floor - 1][idx - 1])
 
     return max(triangle[-1])
 
 
 if __name__ == "__main__":
-    print(
-        solution(
-            [
-                [7],
-                [3, 8],
-                [8, 1, 0],
-                [2, 7, 4, 4],
-                [4, 5, 2, 6, 5],
-            ]
-        )
-    )
-    print(
-        solution(
-            [
-                [7],
-            ]
-        )
-    )
-    print(
-        solution(
-            [
-                [7],
-            ]
-        )
-    )
+    print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
