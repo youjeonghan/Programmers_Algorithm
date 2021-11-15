cf9bec1290438b98854dc82f444e657cf7ab8327
coding_egg <yjh9360@naver.com>
2021-11-16 02:55:37 +0900

    숫자 문자열과 영단어

---
+++ 모든 문제/2021 카카오 채용연계형 인턴십/숫자 문자열과 영단어.py
@@ -0,0 +1,25 @@
+# 레벨1
+
+
+def solution(s):
+    dic = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+
+    for k, v in dic.items():
+        s = s.replace(k, v)
+
+    return int(s)
+
+
+if __name__ == "__main__":
+    print(solution("23four5six7"))
