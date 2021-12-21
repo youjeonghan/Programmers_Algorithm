ed0e5a70547151cb74cdfeed21a9e704ae2ef051
coding_egg <yjh9360@naver.com>
2021-12-22 04:18:23 +0900

    큰 수 만들기 / 다시 품

--- 코딩테스트 고득점 Kit/그리디/큰 수 만들기.py
+++ 코딩테스트 고득점 Kit/그리디/큰 수 만들기.py
@@ -1,16 +1,24 @@
 # 레벨2     탐욕법(Greedy)      큰 수 만들기
-# 시간초과 실패 다시풀기
 
-
-from itertools import combinations
+from collections import deque
 
 
 def solution(number, k):
     l = len(number)
-    li = ["".join(i) for i in combinations(number, l - k)]
-    li.sort()
-    return li[-1]
+    result = deque()
+    for i in number:
+        if not result or result[-1] >= i and l > k:
+            result.append(i)
+        elif result and result[-1] < i:
+            while result and k and result[-1] < i:
+                result.pop()
+                k -= 1
+            result.append(i)
+        l -= 1
+
+    return "".join(result)
 
 
 if __name__ == "__main__":
-    print(solution("1231234", 3))
+    print(solution("1924", 2))
+    print(solution("91", 1))
